214a4daf4740c5b27676ada4222bd7328bc2dddb
Erica <15016253421YH@163.com>
2020-03-26 00:24:12 +0800

    修改status 0 正常 1 删除

--- secondhand/src/main/java/com/secondhand/module/comment/vo/ChildrenCommentVO.java
+++ secondhand/src/main/java/com/secondhand/module/comment/vo/ChildrenCommentVO.java
@@ -1,12 +1,14 @@
 package com.secondhand.module.comment.vo;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author Erica
  * @since 2020/3/25
  */
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class ChildrenCommentVO extends ParentCommentVO {
     // 回复的目标用户
     private Integer toUid;
