0f52030f7330e8ca7454d2ca99bf35f11ea4426f
zhangguijian <844145467@qq.com>
2020-05-05 17:23:38 +0800

    Merge branch 'master' of https://github.com/Asecondhand/secondhand
    # Conflicts:
    #	secondhand/src/main/java/com/secondhand/module/product/service/impl/ProductServiceImpl.java

--- secondhand/src/main/java/com/secondhand/module/product/service/impl/ProductServiceImpl.java
+++ secondhand/src/main/java/com/secondhand/module/product/service/impl/ProductServiceImpl.java
@@ -131,7 +131,7 @@ public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> impl
             try {
                 //向es索引添加数据
                 restHighLevelClient.index(new IndexRequest(EsIndex.PRODUCTINDEX.getIndexName())
-                        .source(JSON.toJSONString(product), XContentType.JSON), RequestOptions.DEFAULT);
+                        .source(JSON.toJSONString(productDTO), XContentType.JSON), RequestOptions.DEFAULT);
                 //通过kafka向消息系统发送数
             } catch (IOException e) {
                 throw new ServiceException("es添加失败");
